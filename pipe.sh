679a7ce5cb146651880b3d19b94017ef998e03d9
Jon Jagger <jon@jaggersoft.com>
2016-12-28 23:06:50 +0000

    switch to a simpler design that cats the main script out of the language_tester image

--- pipe.sh
+++ pipe.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
-set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-# 1.
-# test start-point first
-# get image_name from manifest.json
-# pass it to build_docker_image.sh below
-image_name=cyberdojofoundation/csharp_nunit
+docker run \
+  --rm \
+  cyberdojo/language_tester \
+  sh -c "cat /app/src/bootstrap.sh" | tr -d '\r' > /tmp/bootstrap.sh
 
-# 2. create image
-cid=$(docker run -d cyberdojo/language_tester sh -c "sleep 1d")
-docker cp ${cid}:/app/src/build_docker_image.sh ${my_dir}/build_docker_image.sh
-docker rm -f ${cid}
-${my_dir}/build_docker_image.sh ${image_name}
-
-# 3. test files are the colour they say claim
-#- ./test_src_rags.sh
-
-# 4. test saved output are the colour they claim
-#- ./test_outputs.sh
+chmod +x /tmp/bootstrap.sh
+/tmp/bootstrap.sh ${my_dir}
+status=$?
+rm /tmp/bootstrap.sh
+exit ${status}
