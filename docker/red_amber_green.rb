a23c7f2c12a5c3c1538c327aea78e3e01ae12eaa
Jon Jagger <jon@jaggersoft.com>
2020-01-14 11:03:25 +0000

    Break rag-lambda for momentary test

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -3,5 +3,5 @@ lambda { |stdout,stderr,status|
   output = stdout + stderr
   return :red   if /^\s*Test Count: \d+, Passed: \d+, Failed: [^0]\d*, Warnings: \d+, Inconclusive: \d+, Skipped: \d+/.match(output)
   return :green if /^\s*Overall result: Passed/.match(output)
-  return :amber
+  return :amber sdf
 }
