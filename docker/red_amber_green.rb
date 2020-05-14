23a6d23502ef79ea025b5c9adaa4c9d22cf07e82
Jon Jagger <jon@jaggersoft.com>
2020-05-14 14:03:12 +0100

    Ensure red/amber/green regex pattern gives green when there are Ignored tests

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,7 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  return :red   if /^\s*Test Count: \d+, Passed: \d+, Failed: [^0]\d*, Warnings: \d+, Inconclusive: \d+, Skipped: \d+/.match(output)
-  return :green if /^\s*Overall result: Passed/.match(output)
+  return :green if /^\s*Test Count: \d+, Passed: \d+, Failed: 0, Warnings: \d+, Inconclusive: \d+, Skipped: \d+/.match(output)
+  return :red   if /^\s*Test Count: \d+, Passed: \d+, Failed: \d*, Warnings: \d+, Inconclusive: \d+, Skipped: \d+/.match(output)
   return :amber
 }
