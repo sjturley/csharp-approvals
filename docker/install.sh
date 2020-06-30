2a2ed253a9b6dd25b16be9ae0831c86b30a1244e
Steve Turley <steve@turleylabs.com>
2020-06-30 13:18:38 -0500

    Delete install.sh

--- docker/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -Eeu
-
-mono nuget.exe restore -PackagesDirectory .
-
-mkdir /nunit
-mv NUnit.*/lib                  /nunit
-mv NUnit.ConsoleRunner.*/tools  /nunit
-
-mv red_amber_green.rb /usr/local/bin
